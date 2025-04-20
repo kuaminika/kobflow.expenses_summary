5233d6f71002e23fae617b8aa851f5359d7a595f
kuaminika <kuaminika@gmail.com>
2025-04-20 01:15:21 -0400

    added way to find IP of host that runs this service

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import configs from "./configs.js";
 import Container from "./Container.js";
 import SummaryService from "./SummaryService.js";
 import express from 'express';
-
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
@@ -14,7 +14,18 @@ const repo = container.getArgsForService();
 const service = new SummaryService(repo);
 
 app.get('/', (req, res) => {
+
+
     res.send('Hello from ES Module Express!');
+  });
+
+
+  app.get('/IPData', (req, res) => {
+
+    axios.get('https://api.ipify.org?format=json')
+    .then(res => console.log("Public IP is:", res.data.ip));
+    
+    res.send('It is consoled');
   });
 //container.getArgsForService()
 
