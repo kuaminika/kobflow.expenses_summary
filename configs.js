15dddb8891e6242ef41f7fe8f2102b8ab92800ff
kuaminika <kuaminika@gmail.com>
2025-04-19 23:49:09 -0400

    introduced an  API

--- configs.js
+++ configs.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+const port = process.env.HTTP_PORT;
 const host = process.env.DB_HOST; 
 const user = process.env.DB_USER;
 const database = process.env.DB_NAME;
 const password = process.env.DB_PWD;
 const dbSettings = {host,user,database,password};
 
-export default {...dbSettings}
+export default {...dbSettings,port}
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import configs from "./configs.js";
 import Container from "./Container.js";
+import SummaryService from "./SummaryService.js";
+import express from 'express';
+
+
+const app = express();
+app.use(express.json());
+
 console.log(configs);
 
 const container = new Container(configs);
+const repo = container.getArgsForService();
+const service = new SummaryService(repo);
 
-//TODO do somethign with express and make it become an API or something
+app.get('/', (req, res) => {
+    res.send('Hello from ES Module Express!');
+  });
+//container.getArgsForService()
+
+app.get("/api/summary/latestMonth",async (req,res)=>{
+  
+  let result = await  service.getCurrentMonthlySummary();
+   res.send(result);
+});
 
 
+app.listen(configs.port, () => {
+    console.log(`Server is running at http://localhost:${configs.port}/`);
+  });
