ca5cbfd79a3f552e4d9e4b3a192a6a989e5f5c52
kuaminika <kuaminika@gmail.com>
2025-06-20 02:09:21 -0400

    adding income tests

---
+++ tests_income.js
@@ -0,0 +1,55 @@
+// import express from "express";
+import Container from "./Container.js";
+import DBGatewayTest  from "./Repository/DBGatewayTest.js"
+import RepositoryTester from "./Repository/IncomeSummaryRepositoryTester.js";
+import SummaryServiceTester from "./SummaryServiceTester.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const app = express();
+const {dbGatewayTester }= DBGatewayTest
+const line = "--------------------------------------------------------------------------------------------------------------------------------------------------------------";
+
+
+
+const host = process.env.DB_HOST; 
+const user = process.env.DB_USER;
+const database = process.env.DB_NAME;
+const password = process.env.DB_PWD;
+const dbSettings = {host,user,database,password};
+
+const container = new Container(dbSettings);
+
+ 
+
+// dbGatewayTester.settings = dbSettings;
+// console.log(dbGatewayTester);
+// await dbGatewayTester.testConnect();
+// console.log(line);
+
+// await dbGatewayTester.testQuery("")
+// console.log(line);
+
+// dbGatewayTester.testProcedure();
+
+// const rpTstr = new RepositoryTester(dbSettings);
+// console.log(rpTstr)
+
+
+//  await rpTstr.testGettingForMonths();
+//  console.log(line);
+
+//  await rpTstr.testGettingForCurrentMonth();
+
+
+ 
+const st = new SummaryServiceTester(container,true);
+
+
+ console.log(line);
+
+ await st.testCurrentMonth();
+
+// console.log(line);
+
+// st.testPreviousMonth();
