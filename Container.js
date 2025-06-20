6b5679f08f1ede4b045244a1e0b1808068f9d2da
kuaminika <kuaminika@gmail.com>
2025-06-20 02:05:07 -0400

    updated Container.js tp properly build incomeRepo

--- Container.js
+++ Container.js
@@ -2,6 +2,8 @@
 import DBGateway from "./Repository/DBGateway.js"
 import Repository from "./Repository/ExpenseSummaryRepository.js";
 import IncomeSummaryRepository from "./Repository/IncomeSummaryRepository.js";
+import IncommeSummaryQueryHolder from "./Repository/IncommeSummaryQueryHolder.js";
+ 
  
 
 const {MySQL_DBGateway,DBGatewayArgs}= DBGateway;
@@ -18,18 +20,16 @@ function Container(settings)
         return dbgtw;
     }
 
-    self.getArgsForService_Income = function(user_id){
-        
+    self.getArgsForService_Income = function({user_id}){
+   
         let args = new DBGatewayArgs(settings);
         let dbgtw = new MySQL_DBGateway(args);
         let  queryHolder = new IncommeSummaryQueryHolder();
         let repository = new IncomeSummaryRepository({dbgateWay: dbgtw, user_id : user_id,queryHolder});
         return repository;
-
-
-
     };
-    self.getArgsForService = function(user_id)
+
+    self.getArgsForService = function({user_id})
     {
         let repoArg = self.getArgsForRepository();
         let repo = new Repository(repoArg);
