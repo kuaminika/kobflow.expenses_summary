2923db4cbf7339f1b5bb9d519564e4634d6be7d2
Kuaminika <kuaminika@gmail.com>
2026-01-09 19:40:00 -0500

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "RecordSummaryService",
+      script: "src/index.js",
+
+      // Load .env created by GitHub Actions
+      env_file: ".env",
+
+      // Basic runtime configurations
+      instances: 1,            // or "max" to scale
+      exec_mode: "fork",       // or "cluster"
+      watch: false,            // pm2 should not watch in production
+      autorestart: true,
+      max_memory_restart: "300M",
+
+
+    }
+  ]
+}
